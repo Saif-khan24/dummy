69b8d38f954fe510e7eaedd29e7957fbbf4bcaa4
Saif <kmohdsaif40@gmail.com>
2021-07-20 08:26:42 +0530

    Adding q3

---
+++ q3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class q3 {
+    public static void main(String args[])
+    {
+        System.out.println("yes it worked");
+    }
+}
+
+
+// git add .
+// git commit -m "Add new file"
+// git push origin master
