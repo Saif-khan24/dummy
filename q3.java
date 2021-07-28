f7d3f5896e88e9a2b5e4abf9c4eea11cbafe5075
Saif <kmohdsaif40@gmail.com>
2021-07-28 19:46:34 +0530

    update

--- q3.java
+++ q3.java
@@ -3,7 +3,7 @@ public class q3 {
     public static void main(String args[])
     {
         System.out.println("yes it worked");
-        System.out.println();git 
+        System.out.println(); 
     }
 }
 
